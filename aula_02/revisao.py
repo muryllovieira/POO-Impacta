2b9795f82b14a59e6667f73b5de3a9d0d22c7101
muryllovieira <muryllovieira59@gmail.com>
2024-08-05 22:35:54 -0300

    Revisão e Debug

---
+++ aula_02/revisao.py
@@ -0,0 +1,46 @@
+#TIPOS DE TYPES 
+    # int: 3 -50 80 
+
+    # float: 3.9 -2.6
+
+    # bool: True ou False
+
+    # str: "olá" 'olá' '''olá''' """olá""""
+
+#VARIAVEIS
+    #Variaveis não podem começar com numero
+    #Variaveis só podem tem numeros, letras e _
+
+    # n = 1.2
+    # m = 'olá mundo'
+
+#OPERADORES
+    #Serve para fazer operações 
+        ## Ordem de resolução       Operador            Descrição               Associatividade
+        #         1°                  **              Exponenciação                à direita
+
+        #         2°             +, - (unários)      Identidade e negação          à esquerda
+
+        #         3°              *, /, //, %        Multiplicação, divisão          ||
+        #                                            real, divisão inteira           ||
+        #                                            e resto da  divisão             ||
+
+        #         4°             +, - (binários)      Adição e subtração             ||
+
+        #         5°          ==, !=, >, >=, <, <=   Operadores relacionais       Não associativos
+
+        #         6°                  not               Negação lógica             à esquerda
+
+        #         7°                  and                E lógico                   ||
+
+        #         8°                  or                OU lógico                   ||  
+        ##
+
+#FUNÇÕES
+    #Funções embutidas
+        #print()
+        #input()
+    #Funções criadas
+        # def nome_funcao(p1, p2):   
+        #       return p1 + p2
+        # resultado = nome_funcao(3, 5)
